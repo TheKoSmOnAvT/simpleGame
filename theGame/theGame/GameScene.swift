dd1be24c2bc7fdab4587f1771971c33719ebf727
TheKoSmOnAvT <ni_popov@mail.ru>
2020-07-14 21:53:34 +0500

    add jump

--- theGame/theGame/GameScene.swift
+++ theGame/theGame/GameScene.swift
@@ -18,6 +18,7 @@ class GameScene: SKScene {
     var brickSize = CGSize.zero
     var scrollSpeed: CGFloat = 5.0
     var lastUpdateTime: TimeInterval?
+    let gravitySpeed: CGFloat = 1.5
     
     override func didMove(to view: SKView) {
         anchorPoint = CGPoint.zero
@@ -30,11 +31,33 @@ class GameScene: SKScene {
         
         resetSkater()
         addChild(skater)
+        
+        let tapMethod = #selector(GameScene.handleTap(tapGesture:))
+        let tapGesture = UITapGestureRecognizer(target: self, action: tapMethod)
+        view.addGestureRecognizer(tapGesture)
+        
     }
     
+    @objc func handleTap(tapGesture : UITapGestureRecognizer){
+        if skater.isOnGround {
+            skater.velocity = CGPoint(x: 0.0, y: skater.jumpSpeed)
+            skater.isOnGround = false
+        }
+    }
     
     override func update(_ currentTime: TimeInterval) {
+        var elapsedTime: TimeInterval = 0.0
+        if let lastTimeStamp = lastUpdateTime {
+            elapsedTime = currentTime - lastTimeStamp
+        }
+        lastUpdateTime = currentTime
         
+        let expectedElapsedTime: TimeInterval = 1.0 / 60.0
+        
+        let scrollAdjustment = CGFloat(elapsedTime / expectedElapsedTime)
+        let currentScrollAmount = scrollSpeed * scrollAdjustment
+        updateBricks(withScrollAmount: currentScrollAmount)
+         updateSkater()
     }
     
     func resetSkater(){
@@ -57,7 +80,48 @@ class GameScene: SKScene {
         return brick
     }
     
-    func updateBricks(withScrollAmount currentScrollAmount: CGFloat) {
+    func updateSkater() {
+        if !skater.isOnGround {
+            let velocityY = skater.velocity.y - gravitySpeed
+            skater.velocity = CGPoint(x: skater.velocity.x, y: velocityY)
+            let newSkaterY: CGFloat = skater.position.y +  skater.velocity.y
+            skater.position = CGPoint(x: skater.position.x,  y: newSkaterY)
+        }
         
+        if skater.position.y < skater.minimumY {
+            skater.position.y = skater.minimumY
+            skater.velocity = CGPoint.zero
+            skater.isOnGround = true
+        }
+    }
+    
+    
+    
+    func updateBricks(withScrollAmount currentScrollAmount: CGFloat) {
+        var farthestRightBrickX: CGFloat = 0.0
+         for brick in bricks {
+            let newX = brick.position.x - currentScrollAmount
+            if newX < -brickSize.width {
+                brick.removeFromParent()
+                if let brickIndex = bricks.index(of: brick) {
+                    bricks.remove(at: brickIndex)
+                }
+            } else {
+                brick.position = CGPoint(x: newX, y: brick.position.y)
+                if brick.position.x > farthestRightBrickX { farthestRightBrickX = brick.position.x
+                }
+            }
+        }
+        while farthestRightBrickX < frame.width {
+            var brickX = farthestRightBrickX + brickSize.width + 1.0
+            let brickY = brickSize.height / 2.0
+            let randomNumber = arc4random_uniform(99)
+            if randomNumber < 5 {
+                let gap = 20.0 * scrollSpeed
+                brickX += gap
+            }
+            let newBrick = spawnBrick(atPosition: CGPoint(x: brickX, y: brickY))
+            farthestRightBrickX = newBrick.position.x
+        }
     }
 }
